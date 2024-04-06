aa77723bcc7ed903e8c36df8bf5ac63d15d487ee
Rafael Ochoa Mello <rafaelochoamello@gmail.com>
2024-04-06 19:59:49 -0300

    🔨 Update Post model'n controller, add email, message and photo fields

--- database/migrations/2024_04_06_224517_create_posts_table.php
+++ database/migrations/2024_04_06_224517_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('email');
+            $table->string('message');
+            $table->string('photo');
             $table->timestamps();
         });
     }
