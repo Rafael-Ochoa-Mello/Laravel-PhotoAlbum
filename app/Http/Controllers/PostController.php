de6e154137eadd3c0fb5df844e45604b264ecfc5
Rafael Ochoa Mello <rafaelochoamello@gmail.com>
2024-04-06 22:26:37 -0300

    🔧 Update Post Controller, add store method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Post;
+use App\Models\Post;
+
 
 class PostController extends Controller
 {
     public function index(){
         $posts = Post::all();
         return view('index', ['posts'=>$posts]);
+    }
+
+    public function store(Request $request){
+        $post = new Post();
+        $post->email = $request->email;
+        $post->message = $request->message;
+        $post->photo = ""; //$request->photo;
+        $post->save();
+        return redirect('/');
     }
 }
