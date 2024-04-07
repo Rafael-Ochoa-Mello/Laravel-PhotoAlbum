62419eff9f484f18edd95c74d6a0bcb99e7a5a03
Rafael Ochoa Mello <rafaelochoamello@gmail.com>
2024-04-06 22:26:12 -0300

    🚚 Update route to add store method

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-
-Route::get('/', [PostController::class, 'index']);
-
+Route::get('/', [PostController::class, 'index'])->name('index');
+Route::post('/newPost', [PostController::class, 'store'])->name('post.store');
 
