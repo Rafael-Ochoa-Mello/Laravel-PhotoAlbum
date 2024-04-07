7c4e0f1e87ca805b963e01a474af13dfa6c635f2
Rafael Ochoa Mello <rafaelochoamello@gmail.com>
2024-04-07 00:51:41 -0300

    🚚 Add post delete route

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('index');
 Route::post('/newPost', [PostController::class, 'store'])->name('post.store');
-
+Route::delete('/delete/{id}', [PostController::class, 'destroy'])->name('post.destroy');
